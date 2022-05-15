f7e92c4b5a6962f9a6476794e84c516969de2d2c
Andrew Nedilskej <77886193+andrew18ned@users.noreply.github.com>
2022-05-15 09:30:41 +0300

    Add files via upload

---
+++ drfsite/women/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from django.shortcuts import render
+from .models import Women
+from .serializers import WomenSerializer
+
+
+
+class WomenAPIView(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
