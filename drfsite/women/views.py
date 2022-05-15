bb6d3782d574e2941c59fa629295f27a3243de74
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-15 10:29:56 +0300

    without serializer forming dates and create API

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,7 +9,16 @@ from .serializers import WomenSerializer
 
 class WomenAPIView(APIView):
     def get(self, request):
-        return Response({'title':'andrew17'})
+        list_date = Women.objects.all().values()
+        return Response({'posts':list(list_date)})
+
+    def post(self, request):
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post':model_to_dict(post_new)})
 
 #class WomenAPIView(generics.ListAPIView):
  #   queryset = Women.objects.all()
