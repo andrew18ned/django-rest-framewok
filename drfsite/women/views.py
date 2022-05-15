c23c0cb8e8a68d19a1ee8d8516c43cddd23fc8a4
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-15 09:47:20 +0300

    create method get

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,11 +1,16 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.shortcuts import render
 from .models import Women
 from .serializers import WomenSerializer
 
 
+class WomenAPIView(APIView):
+    def get(self, request):
+        return Response({'title':'andrew17'})
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+#class WomenAPIView(generics.ListAPIView):
+ #   queryset = Women.objects.all()
+ #  serializer_class = WomenSerializer
 
