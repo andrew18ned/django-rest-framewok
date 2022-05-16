f3fdd2588da03e4ea144eba70358175efebd5779
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-16 08:45:58 +0300

    adding methods PUT, POST. update datas in DB

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -16,13 +16,26 @@ class WomenAPIView(APIView):
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
-        return Response({'post':WomenSerializer(post_new).data})
+        serializer.save()
+
+        return Response({'post': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error':'Method PUT not allowed'})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'error':'Objects does not exists'})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid()
+        serializer.save()
+        return Response({'post':serializer.data})
+
+
 
 #class WomenAPIView(generics.ListAPIView):
  #   queryset = Women.objects.all()
