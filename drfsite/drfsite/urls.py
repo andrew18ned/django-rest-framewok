c07aa378963d9f8571dfde4e3af7449bc809af54
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-18 08:25:03 +0300

    setting router and decoratot action for get list categories

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -4,9 +4,9 @@ from rest_framework import routers
 from women.views import *
 
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
-
+router = routers.DefaultRouter()
+router.register(r'women', WomenViewSet, basename='women')
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
