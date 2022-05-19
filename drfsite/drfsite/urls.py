bccea6ae2f56816083e2f048e4fc5e93280680c3
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-19 07:51:32 +0300

    using JWT, access and refresh token. Adding pagination for show dates from DB use restAPI

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 from women.views import *
 
 
@@ -12,5 +17,7 @@ urlpatterns = [
     path('api/v1/womendelete/<int:pk>', WomenAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
-
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication 
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from django.shortcuts import render
 
@@ -13,10 +14,18 @@ from .permission import IsAdminOrReadOnly, IsOwnerOrReadOnly
 
 
 
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3 
+    page_query_param = 'page_size'
+    max_page_size = 10000
+
+
+
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = WomenAPIListPagination
 
 
 
