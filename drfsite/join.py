4b180a9a1f730766b39d26e74571cf91cc2a7a20
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-19 09:04:56 +0300

    left join

---
+++ drfsite/join.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+# absolute url to file
+connection_to_db = sqlite3.connect(r'/home/andrew17/Документы/django-rest-framewok/drfsite/db.sqlite3')
+cursor = connection_to_db.cursor() # for CRUD 
+
+# write query to DB
+cursor.execute('''SELECT title, women_category.name FROM women_women 
+                    LEFT JOIN women_category 
+                        ON women_category.id=women_women.id;''')
+
+result = cursor.fetchall() # one or all record
+print(result)
