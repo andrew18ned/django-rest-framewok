133b3b719d851137763653c37b0929f012097258
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-19 09:08:32 +0300

    finish v1

--- drfsite/join.py
+++ drfsite/join.py
@@ -5,6 +5,7 @@ connection_to_db = sqlite3.connect(r'/home/andrew17/Документы/django-re
 cursor = connection_to_db.cursor() # for CRUD 
 
 # write query to DB
+# SELECT name_fields_table1, name_table2.name_field FROM name_table1 LEFT JOIN name_table_2 ON по яких спільних понях вони будуть об*єднані;
 cursor.execute('''SELECT title, women_category.name FROM women_women 
                     LEFT JOIN women_category 
                         ON women_category.id=women_women.id;''')
